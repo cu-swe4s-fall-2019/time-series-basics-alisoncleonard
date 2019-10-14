5a2f59a64f9a1700d3e95e09ce461b48c6fd309e
alisoncleonard <alisoncleonard@gmail.com>
2019-10-14 11:50:11 -0400

    started unittest file

---
+++ unittests_data_import.py
@@ -0,0 +1,10 @@
+"""
+unit tests for data_import.py class ImportData
+"""
+import unittest
+import data_import
+
+# how do you write unit tests for functions in a class?
+
+class TestImportData(unittest.TestCase):
+    
